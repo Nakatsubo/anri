0de350d9e0816240d32fa066e6ef2c932f10e6c9
NakatsuboYusuke <>
2019-12-09 16:13:51 +0900

    画像アップロードのルーティングとビューを修正しました

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,6 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-  // footer-modal
+  // photo-modal
+  const photo_modal_open = document.getElementById('photo-modal-open');
+  photo_modal_open.addEventListener('click', (e) => {
+    let modal = document.getElementById('photo-modal');
+    $(modal).fadeIn(); // by jQuery
+    return false;
+  }, false);
+
+  const photo_modal_close = document.getElementById('photo-modal-close');
+  photo_modal_close.addEventListener('click', (e) => {
+    let modal = document.getElementById('photo-modal');
+    $(modal).fadeOut(); // by jQuery
+    return false;
+  }, false);
+
+  // footer-modal => 名前をfooter_modal_~に変えておく
   const js_modal_open = document.getElementById('footer-modal-open');
   js_modal_open.addEventListener('click', (e) => {
     let modal = document.getElementById('footer-modal');
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ApplicationRecord
+  validates :photo, presence: true
+
   belongs_to :user
   has_one_attached :photo
 end
