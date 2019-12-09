587e8b912bedbe348736535966129a53ba8f7165
NakatsuboYusuke <>
2019-12-09 11:49:01 +0900

    photoコントローラ、Photoモデルを作成しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_041528) do
+ActiveRecord::Schema.define(version: 2019_12_09_024350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.text "file", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -38,4 +46,5 @@ ActiveRecord::Schema.define(version: 2019_12_04_041528) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "photos", "users"
 end
