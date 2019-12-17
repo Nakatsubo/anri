ae737c958cd3735b61b8f6a201455e41b9cd61f4
NakatsuboYusuke <>
2019-12-17 20:44:37 +0900

    ツイート、リツイートの条件分岐のバグを修正しました

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,11 +7,15 @@ class TweetsController < ApplicationController
       @tweet = Tweet.new(tweet_params)
       @photo = Photo.recent(current_user)
       @photo_uri = set_uri(@photo.photo.blob.key)
-      @set_tweet = set_text(@tweet.text, @tweet.hushtag)
-      @result = AnriTwitter.new(@set_tweet.to_s, @photo_uri.to_s).tweet
-      set_tweet(@tweet, @result, current_user, @photo)
-      if @tweet.save
-        render 'components/posts/posts/index', tweet: @tweet, photo_uri: @photo_uri
+      if @tweet.text.present?
+        @set_tweet = set_text(@tweet.text, @tweet.hushtag)
+        @result = AnriTwitter.new(@set_tweet.to_s, @photo_uri.to_s).tweet
+        set_tweet(@tweet, @result, current_user, @photo)
+        if @tweet.save
+          render 'components/posts/posts/index', tweet: @tweet, photo_uri: @photo_uri
+        else
+          render 'components/posts/posts/error'
+        end
       else
         render 'components/posts/posts/error'
       end
