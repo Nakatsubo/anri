89f2841a79b179992f5344c1f85bcca93bf3de5f
NakatsuboYusuke <>
2019-12-25 17:24:37 +0900

    Heroku上でのGoogleAPIの環境変数の設定を変更しました

--- app/controllers/concerns/error_handler.rb
+++ app/controllers/concerns/error_handler.rb
@@ -6,16 +6,16 @@ module ErrorHandler
     class Forbidden < ActionController::ActionControllerError; end
     class IpAddressRejected < ActionController::ActionControllerError; end
 
-    # rescue_from Exception, with: :rescue500
+    rescue_from Exception, with: :rescue500
     rescue_from Forbidden, with: :rescue403
     rescue_from IpAddressRejected, with: :rescue403
     rescue_from ActiveRecord::RecordNotFound, with: :rescue404
     rescue_from ActionController::RoutingError, with: :rescue404
 
-    # def rescue500(e)
-    #   @exception = e
-    #   render 'errors/internal_server_error', status: 500
-    # end
+    def rescue500(e)
+      @exception = e
+      render 'errors/internal_server_error', status: 500
+    end
 
     def rescue403(e)
       @exception = e
